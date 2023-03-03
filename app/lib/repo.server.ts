2cc74f841db79da5e2fcf0eca8030eba9eb4fe99
Mark Hähnel <hello@markhaehnel.de>
2023-03-03 18:07:55 +0100

    add redis caching

--- app/lib/repo.server.ts
+++ app/lib/repo.server.ts
@@ -1,40 +1,31 @@
-import type { WeekSchedule } from "~/lib/resolvers/scheduleResolver.server";
 import { scheduleResolver } from "~/lib/resolvers/scheduleResolver.server";
 import { streamsResolver } from "~/lib/resolvers/streamsResolver.server";
-import type { StreamsResponse } from "~/lib/twitch/models/StreamsResponseSchema";
+import Redis from "ioredis";
+import superjson from "superjson";
 
-type CachedData = { schedule: WeekSchedule; streams: StreamsResponse["data"] };
+const redis = new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379");
 
-let cache: {
-  schedule: Awaited<ReturnType<typeof scheduleResolver>> | undefined;
-  streams: Awaited<ReturnType<typeof streamsResolver>> | undefined;
-  expiresAt: number;
-} = {
-  schedule: undefined,
-  streams: undefined,
-  expiresAt: 0,
-};
+const CACHE_KEY = "indiestrolche-schedule-cache";
 
-const updateCache = async (): Promise<CachedData> => {
+const updateCache = async () => {
   const userNames = ["marcusbmr", "utzstauder", "internetshawna"];
   const [schedule, streams] = await Promise.all([scheduleResolver(userNames), streamsResolver(userNames)]);
 
-  cache = {
-    schedule,
-    streams,
-    expiresAt: Date.now() + (process.env.NODE_ENV === "production" ? 900_000 : 60_000),
-  };
+  const data = { schedule, streams };
 
-  return { schedule, streams };
+  await redis.set(CACHE_KEY, superjson.stringify(data), "EX", process.env.NODE_ENV === "production" ? 600 : 60);
+
+  return data;
 };
 
 setInterval(async () => {
   await getCachedData();
 }, 60_000);
 
-const getCachedData = async (): Promise<CachedData> => {
-  const { schedule, streams, expiresAt } = cache;
-  return schedule && streams && Date.now() < expiresAt ? { schedule, streams } : updateCache();
+const getCachedData = async () => {
+  const data = await redis.get(CACHE_KEY);
+
+  return data ? superjson.parse(data) : await updateCache();
 };
 
 export { getCachedData };

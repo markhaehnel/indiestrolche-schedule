827a6140f7344af8ab81f914278aa47b0e2830a4
Mark Hähnel <1516205+markhaehnel@users.noreply.github.com>
2023-03-04 10:42:06 +0100

    fix failing redis ipv6 connection (#14)

--- app/lib/resolvers/streamsResolver.server.ts
+++
@@ -1,8 +0,0 @@
-import pipe from "froebel/pipe";
-import { getTwitchAPIClient } from "~/lib/twitch/getTwitchAPIClient.server";
-
-const getStreams = async (userNames: string[]) => getTwitchAPIClient().getStreams(userNames);
-
-const streamsResolver = pipe(getStreams);
-
-export { streamsResolver };
